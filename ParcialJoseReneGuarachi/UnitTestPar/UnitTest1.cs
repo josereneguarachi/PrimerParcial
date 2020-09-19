4dccfd829221eb0191ba409cf0b6f94dc3db763f
joserene123 <joseelunico2000@gmail.com>
2020-09-19 12:34:25 -0400

    creacion del unittest

---
+++ ParcialJoseReneGuarachi/UnitTestPar/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParcialJoseReneGuarachi.Controllers;
+
+namespace UnitTestPar
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGet()
+        {
+            //Arrange
+            CountriesController controlador = new CountriesController();
+
+
+
+            //Act
+            var lista = controlador.GetCountries();
+
+
+            //Assert
+            Assert.IsNotNull(lista);
+        }
+    }
+}
